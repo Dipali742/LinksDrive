75ccb2b54eaf374262e088c0808e6d2b0c70096d
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2021-05-19 18:36:53 +0530

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { loginUser, registerUser } = require('../controllers/auth');
-
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-module.exports = router;
