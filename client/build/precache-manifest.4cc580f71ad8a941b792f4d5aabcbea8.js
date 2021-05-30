8fc2904fb26d42a8f89398aba99d21190ffd916f
Dipali742 <kumbhardipali96@gmail.com>
2021-05-30 18:15:04 +0530

    Changed

---
+++ client/build/precache-manifest.4cc580f71ad8a941b792f4d5aabcbea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e6e6e11597ed7d4a7401b43adaff51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5753ee669b71262455f4",
+    "url": "/static/css/main.e7e35418.chunk.css"
+  },
+  {
+    "revision": "0c2a575f915761cb75d3",
+    "url": "/static/js/2.8be1ee5b.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/static/js/2.8be1ee5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5753ee669b71262455f4",
+    "url": "/static/js/main.396ddfcf.chunk.js"
+  },
+  {
+    "revision": "61e619c5098dc0bb59bd",
+    "url": "/static/js/runtime-main.ca849667.js"
+  }
+]);
