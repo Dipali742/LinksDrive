076492a49fd49905f7194f806a25a43ce135de91
Dipali742 <kumbhardipali96@gmail.com>
2021-06-02 16:16:44 +0530

    addedAbout

---
+++ client/build/precache-manifest.c821eaa97b75050d13b7a9991df8efbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3325b2b0e1859c84cae6a0a13d7fcad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36e452e939eeaf2a6cb8",
+    "url": "/static/css/main.e7e35418.chunk.css"
+  },
+  {
+    "revision": "12d196f77a8288ad13aa",
+    "url": "/static/js/2.707b45bf.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/static/js/2.707b45bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e452e939eeaf2a6cb8",
+    "url": "/static/js/main.dc404b38.chunk.js"
+  },
+  {
+    "revision": "61e619c5098dc0bb59bd",
+    "url": "/static/js/runtime-main.ca849667.js"
+  }
+]);
